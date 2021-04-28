c1a717a9cedad8c0a7b792b6b53b1d4c6df31dd1
username <user@email.mail>
2021-04-28 15:23:38 +0900

    신규 아이디 추천

--- src/App.java
+++ src/App.java
@@ -1,11 +1,13 @@
 import level1.CrainGame;
 import level1.Marathon;
 import level1.PracticeTest;
+import level1.RecommandNewID;
 
 public class App {
     public static void main(String[] args) throws Exception {
         // new CrainGame();
         // new Marathon();
-        new PracticeTest();
+        // new PracticeTest();
+        new RecommandNewID();
     }
 }
