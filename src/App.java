c1d04f51ae68b4ce4474d5dcf0f3f23b4653b3e0
username <user@email.mail>
2021-05-03 13:14:08 +0900

    Main 구조 변경

--- src/App.java
+++ src/App.java
@@ -1,21 +1,52 @@
-import level1.CheckCntN;
-import level1.CrainGame;
-import level1.HateSameNum;
-import level1.Marathon;
-import level1.PracticeTest;
-import level1.RecommandNewID;
-import level1.getMidStr;
-import level1.SumTwoInt;
+import java.io.File;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 public class App {
+
+    /// Fields
+
+    /// Constructor 
+
+    /// Method
+    /**
+     * 
+     * @param packageName d
+     * @param className
+     */
+    public static void classLoader(String packageName, String getClassName) throws Exception{
+
+        // String packageName = "level1";
+        String packageNameSlashed = "./" + packageName.replace(".", "/");
+        System.out.println(packageNameSlashed);
+        URL packageDirURL = Thread.currentThread().getContextClassLoader().getResource(packageNameSlashed);
+
+        String directoryString = packageDirURL.getFile();
+
+        List<Class<?>> classList = new ArrayList<Class<?>>();
+        File directory = new File(directoryString);
+        if(directory.exists()){
+            String[] files = directory.list();
+            for(String fileName : files){
+                fileName = fileName.substring(0, fileName.length() - 6); // 확장자 삭제
+                Class<?> c = Class.forName(packageName + "." + fileName); // Dynamic Loading
+                classList.add(c); // List<Class> list 에 넣는다
+            }
+        }
+
+        String target = packageName + "." + getClassName;
+        for(Class<?> className : classList){
+            if(target.equals(className.getName())){
+                className.getDeclaredConstructor().newInstance();
+            }
+        }
+
+    }
     public static void main(String[] args) throws Exception {
-        // new CrainGame();
-        // new Marathon();
-        // new PracticeTest();
-        // new RecommandNewID();
-        // new CheckCntN();
-        // new getMidStr();
-        // new HateSameNum();
-        new SumTwoInt();
+        
+        // package, class명을 전달하여 클래스 생성하는 메소드 호출
+        classLoader("level1","Ponketmon");
+
     }
 }
---
+++ src/level1/Ponketmon.java
@@ -0,0 +1,28 @@
+package level1;
+
+public class Ponketmon {
+    
+    public Ponketmon() {
+        System.out.println( getClass().getName() + " Create Default Constructor ");
+
+        problem();
+    }
+
+    public void problem() {
+
+        int key = 1;
+        switch (key) {
+            case 1:
+            
+                break;
+        
+            default:
+                break;
+        }
+        
+    }
+
+    public void solution() {
+        
+    }
+}   
