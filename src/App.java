33dbd560bd99d8d8138f46d8ff5ea4046398f7db
박은우 <lostist@MacBook-Pro.local>
2021-05-13 21:23:17 +0900

    맥 os 작업  커밋

--- src/App.java
+++ src/App.java
@@ -8,42 +8,41 @@ public class App {
 
     /// Fields
 
-    /// Constructor 
+    /// Constructor
 
     /// Method
     /**
-     * 앱 실행시 최초 호출하는 클래스 로더 메소드
-     * 사용자에게 입력 받은 클래스의 생성을 담당한다.
+     * 앱 실행시 최초 호출하는 클래스 로더 메소드 사용자에게 입력 받은 클래스의 생성을 담당한다.
      */
     public static void classLoader() throws Exception {
 
         // 처리할 데이터 객체 선언
-        Map<Integer,Class<?>> classMap = new HashMap<Integer,Class<?>>();
+        Map<Integer, Class<?>> classMap = new HashMap<Integer, Class<?>>();
 
         // 패키지 주소 설정
         String packageName = "level1";
         String packageNameSlashed = "./" + packageName.replace(".", "/");
         URL packageDirURL = Thread.currentThread().getContextClassLoader().getResource(packageNameSlashed);
-        
+
         // 주소 설정한 경로에서 파일 얻어오기 위한 경로 세팅
         String directoryString = packageDirURL.getFile();
-        
+
         // 경로에서 읽어 파일로 처리
         File directory = new File(directoryString);
         // 클래스 매핑 처리를 위한 클래스 번호 변수
         int classNo = 1;
         // 경로가 있을 경우에 처리한다
-        if(directory.exists()){
+        if (directory.exists()) {
             // 디렉토리의 파일 리스트를 String[]로 받는다
             String[] files = directory.list();
             // 파일 수 만큼 처리한다.
-            for(String fileName : files){
+            for (String fileName : files) {
                 // 확장자 삭제
-                fileName = fileName.substring(0, fileName.length() - 6); 
+                fileName = fileName.substring(0, fileName.length() - 6);
                 // 클래스 로딩
-                Class<?> clas = Class.forName(packageName + "." + fileName); 
-                // 실행할 파일을 보여주기 위한 클래스 번호와 파일이름                
-                System.out.println( classNo + ". " + fileName );
+                Class<?> clas = Class.forName(packageName + "." + fileName);
+                // 실행할 파일을 보여주기 위한 클래스 번호와 파일이름
+                System.out.println(classNo + ". " + fileName);
                 // 매핑 처리할 데이터맵에 데이터 넣기
                 classMap.put(classNo, clas);
                 classNo++;
@@ -55,14 +54,16 @@ public class App {
         Scanner scan = new Scanner(System.in);
 
         // 입력 받은 클래스의 Default Constructor를 생성한다.
-        classMap.get(scan.nextInt()).getDeclaredConstructor().newInstance();
-
+        // classMap.get(scan.nextInt()).getDeclaredConstructor().newInstance();
+        classMap.get(scan.nextInt()).newInstance();
         // 스캐너가 사용되었으면 닫는다.
-        if(scan!=null) scan.close();
+        if (scan != null)
+            scan.close();
 
     }
+
     public static void main(String[] args) throws Exception {
-        
+
         classLoader();
     }
 }
--- src/level1/Budget.java
+++ src/level1/Budget.java
@@ -1,9 +1,11 @@
 package level1;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Budget {
-    
+
     /// Fields
 
     /// Contructor
@@ -14,54 +16,65 @@ public class Budget {
     }
 
     /// Method
-    public void problem(){
+
+    public static void main(String[] args) throws Exception {
+        new Budget();
+    }
+
+    public void problem() {
 
         System.out.println("￣￣￣￣￣￣￣￣￣￣￣￣￣￣￣");
         System.out.println("1. d[1,3,2,5,4] budget:9");
         System.out.println("2. d[2,2,3,3] budget:10");
         System.out.print("테스트 케이스를 선택 : ");
-        
+
         int[] d = {};
         int budget = 0;
         // 스캐너 사용 후 자동으로 자원 반납을 위한
         // Try-with-resources
-        try(Scanner scan = new Scanner(System.in)){
+        try (Scanner scan = new Scanner(System.in)) {
             int key = scan.nextInt();
             switch (key) {
                 case 1:
-                    d = new int[]{1,3,2,5,4};
+                    d = new int[] { 1, 3, 2, 5, 4 };
                     budget = 9;
                     break;
                 case 2:
-                    d = new int[]{2,2,3,3};
+                    d = new int[] { 2, 2, 3, 3 };
                     budget = 10;
                     break;
                 default:
                     System.out.println("존재하지 않는 테스트 케이스 선택하여 자동으로 1번으로 처리됩니다.");
-                    d = new int[]{1,3,2,5,4};
+                    d = new int[] { 1, 3, 2, 5, 4 };
                     budget = 9;
                     break;
             }
         }
-        
-        int result = solution(d,budget);
+
+        int result = solution(d, budget);
         System.out.println("result : " + result);
     }
 
-    public int solution(int[] d, int budget){
+    public int solution(int[] d, int budget) {
 
         int answer = 0;
+        int sum = 0;
 
+        // 부서별 신청한 금액 오름차순 정렬
+        Arrays.sort(d);
+        // 부서 요청 금액배열의 길이만큼 도는 for문
         for (int i = 0; i < d.length; i++) {
-            System.out.println(budget + " : " + d[i]);
-
-            if(budget - d[i] > 0){
-                budget -= d[i];
-                answer++;
+            // 배열 돌때 마다 카운트 ++
+            answer++;
+            // 합계 금액 더하기
+            sum += d[i];
+            // 만약 합계 금액이 예산보다 큰 경우 카운트 --
+            if (sum > budget) {
+                answer--;
             }
         }
 
-        return answer;        
+        return answer;
     }
 
 }
