8cf55c02d3cde058d3f878b53263c6a16c2f9a14
ieunune <50124623+ieunune@users.noreply.github.com>
2021-05-18 09:05:44 +0900

    비밀지도 풀이 완료

--- src/level1/SecretMap.java
+++ src/level1/SecretMap.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class SecretMap {
     
-     /// Fields
+    /// Fields
 
     /// Contructor
     public SecretMap() {
@@ -16,9 +16,9 @@ public class SecretMap {
     /// Method
     public void problem(){
 
-        System.out.println("=============================================================================== ");
-        System.out.println("1. n:5 arr1[9, 20, 28, 18, 11] arr2[30, 1, 21, 17, 28]                          ");
-        System.out.println("2. n:6 arr1[46, 33, 33 ,22, 31, 50] arr2[27 ,56, 19, 14, 14, 10]                ");
+        System.out.println("===============================================================================");
+        System.out.println("1. n:5 arr1[9, 20, 28, 18, 11] arr2[30, 1, 21, 17, 28]                         ");
+        System.out.println("2. n:6 arr1[46, 33, 33 ,22, 31, 50] arr2[27 ,56, 19, 14, 14, 10]               ");
         System.out.print("테스트 케이스를 선택 : ");
 
         int n = 0;
@@ -49,16 +49,42 @@ public class SecretMap {
         }
 
 
-        solution(n, arr1, arr2);
+        String[] results = solution(n, arr1, arr2);
+        for (String result : results) {
+            System.out.println(result);
+        }
     }
 
     private String[] solution(int n, int[] arr1, int[] arr2) {
 
-        String[] answer = {};
+        // 한변의 길이 n 만큼 답안지의 길이 초기화
+        String[] answer = new String[n];
+        
+        // 한변의 길이 n 만큼 도는 for문
+        for (int i = 0; i < n; ++i) {
 
+            // 문자 처리할 StringBuffer 객체 생성
+            StringBuffer sb = new StringBuffer();
+            // 각 arr1,arr2의 i번째의 숫자 데이터를 Binary |(or) 연산을 하여 answer의 i번째에 대입한다.
+            answer[i] = Integer.toBinaryString(arr1[i] | arr2[i]);
+            
+            // 만약에 한변의 길이가 6인데, Binary의 길이가 5인 경우도 있음으로 공백처리 
+            
+            for (int k = 0; k < n-answer[i].length(); ++k) {
+                sb.append(" ");
+            }
 
-        System.out.println(" arr1[0] | arr2[0]" + Integer.toBinaryString(arr1[0] | arr2[0]));
+            // answer[i]의 길이만큼 돌며 답안지에 세팅할 1-> #, 0-> " " 세팅을 위한 for
+            for (int j = 0; j < answer[i].length(); ++j){
+                if('1' == answer[i].charAt(j)) sb.append("#");
+                else                           sb.append(" ");
+            }
 
+            // 답안지에 처리 완료한 sb 데이터를 대입한다.
+            answer[i] = sb.toString();
+        }
+
+        // 처리 완료 후 전달되는 답안지
         return answer;
     }
 
