036b73ad47a4f254c2e3c940bb19fcc037384df5
ieunune <50124623+ieunune@users.noreply.github.com>
2021-05-26 15:42:10 +0900

    소스 정리

--- src/level1/DotGame.java
+++ src/level1/DotGame.java
@@ -1,6 +1,6 @@
 package level1;
 
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class DotGame {
 
@@ -37,58 +37,126 @@ public class DotGame {
 
         int answer = 0;
 
-        dartResult = dartResult.replaceAll("10","J");
-        StringBuilder display = new StringBuilder();
-        int[] tempJumsuList = new int[3];
-        for (int i = 0; i < dartResult.length(); i++) {
-            int jumsu = 0;
-            int idx = 0;
-            switch (dartResult.charAt(i)) {
-                case 'S':
-                    if(dartResult.charAt(i - 1) == 'J') jumsu = 10;
-                    else jumsu = Character.getNumericValue(dartResult.charAt(i - 1));
-                    answer += Math.pow(jumsu, 1);
-                    // System.out.println("+" + jumsu + "^1");
-                    display.append("+" + jumsu + "^1");
-                    tempJumsuList[idx] = jumsu;
-                    idx++;
+
+        // 토큰 처리
+        StringTokenizer st = new StringTokenizer(dartResult, "SDT*#",true);
+        // 점수 처리할 숫자배열 선언
+        int[] point_arr = new int[3];
+        // 위치(position) 약자 pos 변수 선언
+        int pos = 0;
+
+        // 디버깅 디스플레이 용도의 StringBuilder 선언
+        // StringBuilder display = new StringBuilder();
+
+        // 토큰을 가지고 있는 경우에 수행하는 while 문
+        while(st.hasMoreTokens()){
+
+            // 토큰값을 얻는다
+            String token = st.nextToken();
+
+            // token 값에 따라 case 처리할 switch-case 문
+            switch (token){
+                case "S" :
+                    // S(Single) 일 경우 * 1 이므로 default 에서 배열에 담겨 있기 때문에 연산 불필요
                     break;
-                case 'D':
-                    if(dartResult.charAt(i - 1) == 'J') jumsu = 10;
-                    else jumsu = Character.getNumericValue(dartResult.charAt(i - 1));
-                    answer += Math.pow(jumsu, 2);
-                    // System.out.println("+" + jumsu + "^2");
-                    display.append("+" + jumsu + "^2");
-                    tempJumsuList[idx] = jumsu;
-                    idx++;
+                case "D" :
+                    // D(Double) 일 경우 얻은 점수의 ^2
+                    point_arr[pos-1] = (int) Math.pow(point_arr[pos-1], 2);
+                    // display.append("token : " + token + ", pos : " + pos + ", point_arr[pos-1] : " + point_arr[pos-1] + ", point_arr^2 : " +point_arr[pos-1]+"\n");
                     break;
-                case 'T':
-                    if(dartResult.charAt(i - 1) == 'J') jumsu = 10;
-                    else jumsu = Character.getNumericValue(dartResult.charAt(i - 1));
-                    answer += Math.pow(jumsu, 3);
-                    // System.out.println("+" + jumsu + "^3");
-                    display.append("+" + jumsu + "^3");
-                    tempJumsuList[idx] = jumsu;
-                    idx++;
+                case "T" :
+                    // T(Triple) 일 경우 얻은 점수의 ^3
+                    point_arr[pos-1] = (int) Math.pow(point_arr[pos-1], 3);
+                    // display.append("token : " + token + ", pos : " + pos + ", point_arr[pos-1] : " + point_arr[pos-1] + ", point_arr^3 : " +point_arr[pos-1]+"\n");
                     break;
-                case '*':
-                    int temp = tempJumsuList[idx] * 2;
-                    temp += tempJumsuList[idx-1] *2;
-                    answer = temp;
-                    // System.out.println(answer + "*2");
+                case "*" :
+                    // *(스타상) 일 경우 얻은 점수 2배
+                    point_arr[pos-1] = point_arr[pos-1] * 2;
+                    // 만약 스타상의 위치가 2이상 인 경우에 앞 라운드의 점수를 * 2 처리한다.
+                    if(pos > 1) point_arr[pos-2] = point_arr[pos-2] * 2;
+                    // display.append("token : " + token + ", pos : " + pos + ", point_arr[pos-1] : " + point_arr[pos-1] + ", point_arr*2 : " +point_arr[pos-1] * 2+"\n");
                     break;
-                case '#':
-                    answer = answer * (-1);
-                    // System.out.println(answer + "*(-1)");
-                    display.append("*(-1)");
+                case "#" :
+                    // #(아차상) 일 경우 얻은 점수 * -1
+                    point_arr[pos-1] = point_arr[pos-1] * -1;
+                    // display.append("token : " + token + ", pos : " + pos + ", point_arr[pos-1] : " + point_arr[pos-1] + ", point_arr-1 : " +point_arr[pos-1] * 2+"\n");
                     break;
+                default:
+                    // 각 라운드 별 점수 처리하는 default 구문
+                    point_arr[pos] = Integer.parseInt(token);
+                    // display.append("token : " + token + ", pos : " + pos + ", point_arr[pos-1] : " + point_arr[pos] + ", point_arr : " +point_arr[pos]+"\n");
+                    pos++;
             }
+
         }
-        System.out.println(display);
-        System.out.println("===================================");
+
+        // 처리 완료한 포인트 배열을 합산한다.
+        for (int getPoint: point_arr) {
+            answer += getPoint;
+        }
+
+        // display.append("game : " + dartResult +  ", answer : " + answer);
+        // System.out.println(display);
+        // System.out.println("===================================");
 
         return answer;
     }
+//    public int solution(String dartResult) {
+//
+//        int answer = 0;
+//
+//        dartResult = dartResult.replaceAll("10", "J");
+//        StringBuilder display = new StringBuilder();
+//        int[] tempJumsuList = new int[3];
+//        for (int i = 0; i < dartResult.length(); i++) {
+//            int jumsu = 0;
+//            int idx = 0;
+//            switch (dartResult.charAt(i)) {
+//                case 'S':
+//                    if (dartResult.charAt(i - 1) == 'J') jumsu = 10;
+//                    else jumsu = Character.getNumericValue(dartResult.charAt(i - 1));
+//                    answer += Math.pow(jumsu, 1);
+//                    // System.out.println("+" + jumsu + "^1");
+//                    display.append("+" + jumsu + "^1");
+//                    tempJumsuList[idx] = jumsu;
+//                    idx++;
+//                    break;
+//                case 'D':
+//                    if (dartResult.charAt(i - 1) == 'J') jumsu = 10;
+//                    else jumsu = Character.getNumericValue(dartResult.charAt(i - 1));
+//                    answer += Math.pow(jumsu, 2);
+//                    // System.out.println("+" + jumsu + "^2");
+//                    display.append("+" + jumsu + "^2");
+//                    tempJumsuList[idx] = jumsu;
+//                    idx++;
+//                    break;
+//                case 'T':
+//                    if (dartResult.charAt(i - 1) == 'J') jumsu = 10;
+//                    else jumsu = Character.getNumericValue(dartResult.charAt(i - 1));
+//                    answer += Math.pow(jumsu, 3);
+//                    // System.out.println("+" + jumsu + "^3");
+//                    display.append("+" + jumsu + "^3");
+//                    tempJumsuList[idx] = jumsu;
+//                    idx++;
+//                    break;
+//                case '*':
+//                    int temp = tempJumsuList[idx] * 2;
+//                    temp += tempJumsuList[idx - 1] * 2;
+//                    answer = temp;
+//                    // System.out.println(answer + "*2");
+//                    break;
+//                case '#':
+//                    answer = answer * (-1);
+//                    // System.out.println(answer + "*(-1)");
+//                    display.append("*(-1)");
+//                    break;
+//            }
+//        }
+//        System.out.println(display);
+//        System.out.println("===================================");
+
+//        return answer;
+//    }
 
 
 }
