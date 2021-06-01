88514fae7f0b0938033a91c225b4ccc996ebc8fe
ieunune <50124623+ieunune@users.noreply.github.com>
2021-06-01 18:25:44 +0900

    [진행중]RemoveMin.java [완]CollatzGuess.java [완]LottoRank.java

--- src/level1/LottoRank.java
+++ src/level1/LottoRank.java
@@ -63,16 +63,46 @@ public class LottoRank {
             }
         }
         System.out.println("====================================");
-        int[] result = solution(lottos, win_nums);
+        int[] results = solution(lottos, win_nums);
 
-        System.out.print("result : " + result);
+        StringBuilder sb = new StringBuilder();
+        sb.append("result [");
+        for (int result: results) {
+            sb.append(result + ",");
+        }
+        sb.append("]");
+        System.out.print(sb.toString());
 
     }
 
     public int[] solution(int[] lottos, int[] win_nums){
 
-        int[] answer = {};
+        // 최고, 최저 세팅할 2의 배열길이로 선언
+        int[] answer = new int[2];
 
+        // 일치개수와 0의 개수 측정
+        int match_cnt = 0;
+        int zero_cnt = 0;
+
+        // lottos 의 길이만큼 도는 outer for
+        for (int i = 0; i < lottos.length; i++) {
+            // 0인 개수 측정
+            if(lottos[i] == 0) zero_cnt++;
+            // win_nums 의 길이만큼 도는 inner for
+            for (int j = 0; j < win_nums.length; j++) {
+                // 일치하는 번호가 있는경우 일치개수 ++
+                if(win_nums[j] == lottos[i]){
+                    match_cnt++;
+                    break;
+                }
+            }
+        }
+
+        // System.out.println("[DEBUG] zero cnt : " + zero_cnt + ", match_cnt : " + match_cnt);
+        // 최고 순위 세팅
+        answer[0] = Math.min(7-(zero_cnt+match_cnt),6);
+        // 최저 순위 세팅
+        answer[1] = Math.min(7-match_cnt,6);
 
         return answer;
     }
